12ac904bd3af83c1ca6b9746bb2115eedf194e42
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2020-07-27 17:01:37 +0500

    done with task

--- app/src/main/java/com/thefuturestic/dynamicsurfacedemo/MainActivity.kt
+++ app/src/main/java/com/thefuturestic/dynamicsurfacedemo/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.thefuturestic.dynamicsurfacedemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        (findViewById<View>(R.id.collapsing_toolbar_layout) as CollapsingToolbarLayout).setTitle("Eclairs")
     }
 }
